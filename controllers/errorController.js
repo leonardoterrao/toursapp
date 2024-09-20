c282b6c9ec71ad4e16bfdca824cc71e78e67d8bb
leonardoterrao <leonardoterrao@gmail.com>
2024-09-19 22:33:40 -0300

    handling duplicated database fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,12 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicate field value ${value}. Please use another value.`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -39,10 +45,8 @@ module.exports = (err, req, res, next) => {
     sendErrorProd(err, res);
   } else {
     let error = { ...err };
-    if (err.name === 'CastError') {
-      console.log('Unexpected Error');
-      error = handleCastErrorDB(error);
-    }
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
 
     sendErrorDev(error, res);
   }
