db466c3f96654d1edd275e2f703a199411d14441
leonardoterrao <leonardoterrao@gmail.com>
2024-09-23 20:32:19 -0300

    handling mongoose validation errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -6,8 +6,14 @@ const handleCastErrorDB = (err) => {
 };
 
 const handleDuplicateFieldsDB = (err) => {
-  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
-  const message = `Duplicate field value ${value}. Please use another value.`;
+  console.log(err);
+  const message = `Duplicate field value '${err.keyValue.name}'. Please use another value.`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
   return new AppError(message, 400);
 };
 
@@ -42,12 +48,14 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
-  } else {
     let error = { ...err };
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
 
-    sendErrorDev(error, res);
+    sendErrorProd(error, res);
+  } else {
+    sendErrorDev(err, res);
   }
 };
