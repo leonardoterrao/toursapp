9d11b0f4a86ffe483b96d1ad889538672bddf145
leonardoterrao <leonardoterrao@gmail.com>
2024-09-24 22:46:12 -0300

    protecting tour routes

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -1,3 +1,4 @@
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
@@ -44,4 +45,39 @@ exports.login = catchAsync(async (req, res, next) => {
     status: 'success',
     token,
   });
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // get token and check of it exists
+  let token;
+  const { authorization } = req.headers;
+  if (authorization && authorization.startsWith('Bearer')) {
+    token = authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(new AppError('You are not logged in. Please log in', 401));
+  }
+
+  //  verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  // checm if user still exists
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new AppError('The user belonging to this token no longer exist.', 401),
+    );
+  }
+
+  // check if user changed password after the token was issued
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('User has changed password. Please log in again.', 401),
+    );
+  }
+
+  // grant access to protected route
+  req.user = currentUser;
+  next();
 });
