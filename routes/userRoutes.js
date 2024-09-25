96d6c08e4f548cca9a84f3755d753eead763f12a
leonardoterrao <leonardoterrao@gmail.com>
2024-09-24 21:59:41 -0300

    logging in users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,7 @@ const authController = require('../controllers/authenticationController');
 const router = express.Router();
 
 router.post('/signup', authController.signup);
+router.post('/login', authController.login);
 
 router
   .route('/')
