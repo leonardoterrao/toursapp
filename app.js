32408cf8cc4362b0e1d7c7b7beabc15d43f9f28c
leonardoterrao <leonardoterrao@gmail.com>
2024-09-26 22:32:15 -0300

    data sanitization

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const ratelimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -27,6 +29,12 @@ const limiter = ratelimit({
 
 // limit requests from same API
 app.use('/api', limiter);
+
+// data sanitization agains NoSQL query injection
+app.use(mongoSanitize());
+
+// data sanitization agains XSS
+app.user(xss());
 
 // body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
