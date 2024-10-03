29fedea7660046f0d6ca7956ff45dfb370b5373f
leonardoterrao <leonardoterrao@gmail.com>
2024-10-02 21:50:25 -0300

    adding missing authentication and authorization

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -4,11 +4,12 @@ const authController = require('../controllers/authenticationController');
 
 const router = express.Router({ mergeParams: true });
 
+router.use(authController.protect);
+
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview,
@@ -17,7 +18,13 @@ router
 router
   .route('/:id')
   .get(reviewController.getOneReview)
-  .delete(authController.protect, reviewController.deleteReview)
-  .patch(authController.protect, reviewController.updateReview);
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewController.deleteReview,
+  )
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview,
+  );
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,21 +11,15 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword,
-);
+// protect all routes after this middleware
+router.use(authController.protect);
 
-router.get(
-  '/getMe',
-  authController.protect,
-  userController.getMe,
-  userController.getUser,
-);
+router.patch('/updateMyPassword', authController.updatePassword);
+router.get('/getMe', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
 
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+router.use(authController.restrictTo('admin'));
 
 router
   .route('/')
