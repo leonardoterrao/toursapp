1c4a1461d83f1a6db13e2a3dc233e99fca9d0328
leonardoterrao <leonardoterrao@gmail.com>
2024-10-07 21:53:18 -0300

    setting up pug with express

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const ratelimit = require('express-rate-limit');
@@ -13,6 +14,9 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 // GLOBAL MIDDLEWARES
 // set security HTTP headers
@@ -42,7 +46,7 @@ app.use(xss());
 app.use(express.json({ limit: '10kb' }));
 
 // serve static files from the public directory (for images, css, etc.)
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, '/public')));
 
 // prevent parameter pollution
 app.use(
@@ -62,6 +66,10 @@ app.use(
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
+});
+
+app.get('/', (req, res) => {
+  res.status(200).render('base');
 });
 
 app.use('/api/v1/tours', tourRouter);
