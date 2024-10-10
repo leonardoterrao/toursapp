98aea1d96661ef326361da063def2a639e7bd410
leonardoterrao <leonardoterrao@gmail.com>
2024-10-09 22:56:43 -0300

    building tour page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -3,8 +3,6 @@ const catchAsync = require('../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res) => {
   const tours = await Tour.find();
-  
-  
 
   res.status(200).render('overview', {
     title: 'All Tours',
@@ -12,8 +10,16 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  const tour = await Tour.findOne({ slug: req.params.slug })
+    .populate({
+      path: 'reviews',
+      fields: 'review rating user',
+    })
+    .populate({ path: 'guides', fields: 'name role photo' });
+
   res.status(200).render('tour', {
-    title: 'The Forest Hiker',
+    title: tour.name,
+    tour,
   });
-};
+});
