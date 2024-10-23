06a92b9f693ee80ba5210338cad0469efa2ffca5
leonardoterrao <leonardoterrao@gmail.com>
2024-10-22 23:31:10 -0300

    responding to a sigterm signal

--- server.js
+++ server.js
@@ -40,3 +40,10 @@ process.on('uncaughtException', (err) => {
     process.exit(1);
   });
 });
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM. Shutting down gracefully');
+  server.close(() => {
+    console.log('Process Terminated!');
+  });
+});
